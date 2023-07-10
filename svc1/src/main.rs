b1929640f8bb44ea796e694ebb7e4241dc5e7c04
MarcioJales <marcio.dojcosta@gmail.com>
2023-07-09 21:12:58 -0300

    Http exporter

--- svc1/src/main.rs
+++ svc1/src/main.rs
@@ -1,5 +1,11 @@
 use opentelemetry::{
-    sdk::runtime,
+    sdk::{
+        runtime,
+        trace::{
+            self,
+            Sampler,
+        }
+    },
     trace::Tracer,
 };
 
@@ -9,19 +15,22 @@ use std::time::Duration;
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
 
     let export_config = opentelemetry_otlp::ExportConfig {
-        endpoint: "http://localhost:4317".to_string(),
+        endpoint: "http://localhost:4318".to_string(),
         timeout: Duration::from_secs(3),
-        protocol: Protocol::Grpc
+        protocol: Protocol::HttpBinary
     };
 
     let otlp_exporter_build = opentelemetry_otlp::new_exporter()
-    .grpcio()
+    .http()
     .with_export_config(export_config);
     
-    let otlp_tracer = opentelemetry_otlp::new_pipeline().
-    tracing().
-    with_exporter(otlp_exporter_build).
-    install_batch(runtime::AsyncStd).unwrap();
+    let otlp_tracer = opentelemetry_otlp::new_pipeline()
+    .tracing()
+    .with_exporter(otlp_exporter_build)
+    .with_trace_config(
+        trace::config()
+            .with_sampler(Sampler::AlwaysOn))
+    .install_batch(runtime::AsyncStd).unwrap();
 
     otlp_tracer.in_span("doing_work", |cx| {
         println!("Poing");
