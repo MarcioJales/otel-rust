6f045fb2a22839fc11345c0ecb6bdbcc9b23fb64
MarcioJales <marcio.dojcosta@gmail.com>
2023-10-29 20:07:14 -0300

    Adapt to the example on SigNoz https://signoz.io/blog/opentelemetry-rust/

--- svc1/src/main.rs
+++ svc1/src/main.rs
@@ -1,5 +1,6 @@
 use opentelemetry::{
     sdk::{
+        Resource,
         runtime,
         trace::{
             self,
@@ -9,26 +10,19 @@ use opentelemetry::{
     trace::Tracer,
 };
 
-use opentelemetry_otlp::{Protocol, WithExportConfig};
-use std::time::Duration;
+use opentelemetry_otlp::WithExportConfig;
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-
-    let export_config = opentelemetry_otlp::ExportConfig {
-        endpoint: "http://localhost:4318".to_string(),
-        timeout: Duration::from_secs(3),
-        protocol: Protocol::HttpBinary
-    };
-
     let otlp_exporter_build = opentelemetry_otlp::new_exporter()
-    .http()
-    .with_export_config(export_config);
+    .tonic()
+    .with_env();
     
     let otlp_tracer = opentelemetry_otlp::new_pipeline()
     .tracing()
     .with_exporter(otlp_exporter_build)
     .with_trace_config(
         trace::config()
+            .with_resource(Resource::default())
             .with_sampler(Sampler::AlwaysOn))
     .install_batch(runtime::AsyncStd).unwrap();
 
