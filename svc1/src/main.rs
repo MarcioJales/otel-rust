cabf874b1ab2bd293c9bb6fad433040c4a3199aa
MarcioJales <marcio.dojcosta@gmail.com>
2023-11-12 23:27:27 -0300

    Change approach to use TraceProvider and SpanExporter.
    https://opentelemetry.io/docs/instrumentation/rust/exporters/
    
    https://github.com/open-telemetry/opentelemetry-rust#getting-started

--- svc1/src/main.rs
+++ svc1/src/main.rs
@@ -1,37 +1,26 @@
 use opentelemetry::{
     Context,
-    sdk::{
-        Resource,
-        runtime,
-        trace::{
-            self,
-            Sampler,
-        }
-    },
+    sdk::trace::TracerProvider,
     trace::{
+        TracerProvider as _,
+        TraceContextExt,
         Tracer,
-        TraceContextExt
     }
 };
 
-use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_otlp::{SpanExporter, ExportConfig, TonicConfig};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let otlp_exporter_build = opentelemetry_otlp::new_exporter()
-    .tonic()
-    .with_env();
-    
-    let otlp_tracer = opentelemetry_otlp::new_pipeline()
-    .tracing()
-    .with_exporter(otlp_exporter_build)
-    .with_trace_config(
-        trace::config()
-            .with_resource(Resource::default())
-            .with_sampler(Sampler::AlwaysOn))
-    .install_batch(runtime::Tokio).unwrap();
+    let exporter = SpanExporter::new_tonic(ExportConfig::default(), TonicConfig::default())?;
+
+    let provider = TracerProvider::builder()
+    .with_simple_exporter(exporter)
+    .build();
+
+    let tracer = provider.tracer("otlp_eu");
         
-    let parent = otlp_tracer.start("poing");
+    let parent = tracer.start("poing");
     println!("{:?}", parent);
     let parent_cx = Context::current_with_span(parent);
     println!("{:?}", parent_cx);
