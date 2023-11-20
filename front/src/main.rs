90d3d81e066d4a4d7b720e32a78a58427ef261ec
MarcioJales <marcio.dojcosta@gmail.com>
2023-11-19 22:20:53 -0300

    Add a TCP Listener and add basic HTML build from example

--- front/src/main.rs
+++ front/src/main.rs
@@ -10,6 +10,10 @@ use opentelemetry::{
 
 use opentelemetry_otlp::{SpanExporter, ExportConfig, TonicConfig};
 
+use build_html::{self, Html, HtmlContainer};
+
+use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
     let exporter = SpanExporter::new_tonic(ExportConfig::default(), TonicConfig::default())?;
@@ -18,13 +22,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     .with_simple_exporter(exporter)
     .build();
 
-    let tracer = provider.tracer("otlp_eu");
+    let tracer = provider.tracer("front");
         
-    let parent = tracer.start("poing");
-    println!("{:?}", parent);
-    let parent_cx = Context::current_with_span(parent);
-    println!("{:?}", parent_cx);
-    println!("Poing");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        let parent = tracer.start("listener");
+        println!("Connection established!");
+    }
+
+    let page = build_html::HtmlPage::new()
+    .with_paragraph("Some Text")
+    .to_html_string();
     
     Ok(())
 }
